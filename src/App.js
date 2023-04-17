2dae3d1b2ee10aebdac4059cc52c6dff969f10d7
amritanshu001 <91420206+amritanshu001@users.noreply.github.com>
2023-04-17 18:21:11 +0530

    adding state for cart modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartStatus, setCartStatus] = useState(false);
+
+  const showCartHandler = () => {
+    setCartStatus(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartStatus(false);
+  };
+
   return (
     <CartProvider>
       <Header />
